a4a12c72ffdd791135c0a9ebca04df9bd0db8cb4
MATSUBAYASHI 'Shaolin' Kohji <shaolin@vinecaves.com>
2013-01-24 08:55:13 +0900

    issue-4316: プレビューなし印刷機能自体の実装、設定機能はまだ

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ EnjuLeaf::Application.routes.draw do
     resources :series_has_manifestations
     resources :reserves
     post :output_show, :on => :member
+    get :output_pdf, :on => :member
   end
 
   resources :library_checks do
@@ -288,6 +289,7 @@ EnjuLeaf::Application.routes.draw do
 
   resources :reserves do
     post :output, :on => :member
+    get :output_pdf, :on => :member
     get :retain, :on => :collection
     post :retain_item, :on => :collection
     post :informed, :on => :member
